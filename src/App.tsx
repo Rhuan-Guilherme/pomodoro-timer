1a625ea2ea49357cbac2867829d44103325e90a3
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-11-20 18:55:33 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
 import { defaultTheme } from "./styles/default"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button variant="secundary" />
+        <Button />
+        <Button variant="danger" />
+        <Button variant="success" />
+        <Button variant="secundary" />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
