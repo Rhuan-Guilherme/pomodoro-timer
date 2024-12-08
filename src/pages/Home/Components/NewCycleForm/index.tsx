852489176950f8587797e5dac0b0b8f90f417897
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-08 12:37:44 -0300

    Removendo listagem do input de projetos

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -14,17 +14,9 @@ export function NewCycleForm() {
         id="task"
         type="text"
         placeholder="Dê um nome para o seu projeto"
-        list="task-suggestions"
         disabled={!!activeCycle}
         {...register('task')}
       />
-
-      <datalist id="task-suggestions">
-        <option value="Projeto 1" />
-        <option value="Projeto 2" />
-        <option value="Projeto 3" />
-        <option value="Projeto 4" />
-      </datalist>
 
       <label htmlFor="minutesAmmount">Durante</label>
       <MinutesAmmountInput
